ab5a8a91517176176f6bc498f00d1739dcb035c4
Thanh Quang Do <69701708+ThanhQuangDo@users.noreply.github.com>
2021-07-20 14:29:53 +0700

    fix view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,16 +12,7 @@
     <link href="{{URL::asset('404/css/price-range.css')}}" rel="stylesheet">
     <link href="{{URL::asset('404/css/animate.css')}}" rel="stylesheet">
 	<link href="{{URL::asset('404/css/main.css')}}" rel="stylesheet">
-	<link href="{{URL::asset('404/css/responsive.css')}}" rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+	<link href="{{URL::asset('404/css/responsive.css')}}" rel="stylesheet">  
 </head><!--/head-->
 
 <body>
@@ -36,8 +27,5 @@
             <h2><a href="{{route('/')}}">Bring me back Home</a></h2>
 		</div>
 	</div>
-
-  
-   
 </body>
 </html>
