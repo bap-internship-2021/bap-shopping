afba9a0db5509429b0a35db7be27203f9236bdfe
Thanh Quang Do <69701708+ThanhQuangDo@users.noreply.github.com>
2021-07-27 10:47:58 +0700

    forgot and reset password using mail

--- config/mail.php
+++ config/mail.php
@@ -83,9 +83,17 @@ return [
     |
     */
 
+    'stream' => [
+        'ssl' =>[
+            'allow_self_signed' =>true,
+            'verify_peer' =>false,
+            'verify_peer_name' =>false,
+        ],
+    ],
+
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'quangdt1603@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'BAP-SHOP'),
     ],
 
     /*
