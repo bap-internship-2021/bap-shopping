d2fb304a8bdb1dbd44d90b26308337246b2e4b4d
Thanh Quang Do <69701708+ThanhQuangDo@users.noreply.github.com>
2021-07-27 17:41:53 +0700

    fix UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="{{asset('admin/images/Bap-logo.jpg')}}" class="w-80 h-80 rounded-full">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
@@ -32,16 +32,26 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+            <div class="flex items-center justify-between mt-4">
+                <div class="p-2">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 text-left" href="{{ route('register') }}">
+                        {{ __('Register') }}
+                    </a>
+                </div>
+               
+                <div>
+                    @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
-                @endif
+                    @endif
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
+                    <x-jet-button class="ml-4">
+                        {{ __('Log in') }}
+                    </x-jet-button>
+                </div>
+            
+                
             </div>
         </form>
     </x-jet-authentication-card>
